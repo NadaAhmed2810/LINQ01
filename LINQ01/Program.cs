7231c689e4549735a8e44cfdcb89ba4939dd4eb9
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-29 18:32:30 +0200

    Implicitly Typed Local variable

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Implicitly Typed Local variable
+            #region Var
+            ////Var : Is a C# keyword [C# 3.0 feature]
+            //var Name = "Ahmed";
+            ////Compiler Can detect The type of variable on its Initial Value at a complier
+            ////Must be Initialized
+            ////Can not  be Initialized with Null
+            ////After Initialization, We Can not Change the DataType
+            ////new object  =>Complier Generate in il
+            ////type safe and can not effect on performance 
+            //var Employee = new { Id = 10, Name = "Nour" };
+            //Console.WriteLine(Employee.Id);
+            //Console.WriteLine(Employee.Age);
+
+            #endregion
+            #region Dynamic 
+            //dynamic; is a c# keyword [C# 4.0 Feature]
+            // dynamic Data = "Nada";
+            //Complier Will Escape Type Checking at Compilation Timw=e
+            //CLR will Resolve the actual type of a Dynamic Variable at Run time and Will be changed Based on assign value 
+            // Not must be initialized 
+            // Can be initializes with NuLL
+            //After Initialization,U Can Change its DataType
+            //Data = 10;
+            //Data = 10.5;
+            //Data = true;
+            //Data = 'a';
+            //dynamic x = null;
+            //Console.WriteLine(x);
+            //dynamic Employee = new { Id = 10, Name = "Nour" };
+            //Console.WriteLine(Employee.Id);
+            //Console.WriteLine(Employee.Age);
+
+
+            #endregion
+            #endregion
         }
     }
 }
