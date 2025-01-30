e658c72190cdac15fccee04ef9d6a8b722b17d46
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-30 13:05:58 +0200

    Q2:. Find all products that are in stock and cost more than 3.00 per unit.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -19,6 +19,18 @@ namespace Assignment
             //    Console.WriteLine(Product);
             //}
             #endregion
+            #region Q2:. Find all products that are in stock and cost more than 3.00 per unit.
+            //var Result = ProductList.Where(P => P.UnitsInStock > 0 && P.UnitPrice > 3);
+            //Result = from P in ProductList
+            //         where P.UnitsInStock > 0 && P.UnitPrice>3
+            //         select P;
+
+            //foreach (var Product in Result)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+            #endregion
             #endregion
 
         }
