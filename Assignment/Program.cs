6e243ca0c3a25090261c1e2e6528a1eb03d71489
NadaAhmed2810 <nwrabsywny534@gmail.com>
2025-01-30 14:21:02 +0200

    Q5:Sort first by word length and then by a case-insensitive sort of the words in an array.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -58,40 +58,54 @@ namespace Assignment
 
             #endregion
             #region Q2: Uses a custom comparer to do a case-insensitive sort of the words in an array.
-            string[] Words = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr" , "cHeRry" };
-            var Result = Words.OrderBy(W =>W.ToLower());
-            foreach (var word in Result)
-            {
-                Console.WriteLine(word);
-            }
+            //string[] Words = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr" , "cHeRry" };
+            //var Result = Words.OrderBy(W =>W.ToLower());
+            //foreach (var word in Result)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+
+            #endregion
+            #region Q3:Sort a list of products by units in stock from highest to lowest.
+            //var Result = ProductList.OrderByDescending(P => P.UnitsInStock);
+            //Result= from P in ProductList
+            //        orderby P.UnitsInStock descending
+            //        select P;
+
+            //foreach (var Product in Result)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+
+
+            #endregion
+            #region Q4:Sort a list of digits, first by length of their name, and then alphabetically by the name itself.
+            //string [] Array = { "zero", "one","two","three","four","five","six","seven","eight","nine" };
+            //var Result = Array.OrderBy(D => D.Length).ThenBy(D => D);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+            #region Q5:Sort first by word length and then by a case-insensitive sort of the words in an array.
+            //string[] words = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var Result =words.OrderBy(W=>W.Length)
+            //                 .ThenBy(W=>W.ToLower());
+            //Result=from W in words
+            //       orderby W.Length ,W.ToLower()
+            //       select W;
+            //foreach (var word in Result)
+            //{
+            //    Console.WriteLine(word);
+            //}
+
+            #endregion
+
+            #endregion
 
         }
-        #endregion
-        #region Q3:Sort a list of products by units in stock from highest to lowest.
-        //var Result = ProductList.OrderByDescending(P => P.UnitsInStock);
-        //Result= from P in ProductList
-        //        orderby P.UnitsInStock descending
-        //        select P;
-
-        //foreach (var Product in Result)
-        //{
-        //    Console.WriteLine(Product);
-        //}
-
-
-        #endregion
-        #region Q4:Sort a list of digits, first by length of their name, and then alphabetically by the name itself.
-        //string [] Array = { "zero", "one","two","three","four","five","six","seven","eight","nine" };
-        //var Result = Array.OrderBy(D => D.Length).ThenBy(D => D);
-        //foreach (var item in Result)
-        //{
-        //    Console.WriteLine(item);
-        //}
-
-        #endregion
-
-        #endregion
-
     }
 }
 
